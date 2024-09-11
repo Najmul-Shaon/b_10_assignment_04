2e7244d0190c1ecb46c4c4b5be82e230f7d7f73c
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-09-12 00:02:21 +0600

    problem one

--- taxCalculator.js
+++ taxCalculator.js
@@ -1,1 +1,20 @@
-console.log("hi");
+function calculateTax(income, cost) {
+  if (
+    isNaN(income) ||
+    isNaN(cost) ||
+    0 >= income ||
+    0 >= cost ||
+    cost >= income
+  ) {
+    return "Invalid String";
+  } else {
+    const diff = income - cost;
+      const taxOfTweentyPercent = (diff * .20);
+    return taxOfTweentyPercent;
+  }
+}
+
+const monthly_income = 5000;
+const monthly_cost = 1500;
+const result = calculateTax(monthly_income, monthly_cost);
+console.log(result);
