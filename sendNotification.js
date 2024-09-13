144d68237c3bb24c0c96380e491e54cc69ca0cb4
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-09-13 23:34:07 +0600

    solved problem 2

--- sendNotification.js
+++ sendNotification.js
@@ -1,7 +1,12 @@
 function sendNotification(email) {
+  const isIclude = email.indexOf("@");
+  if (isIclude < 0) {
+    return "Invalid Email";
+  } else {
     const splitEmail = email.split("@");
     const message = `${splitEmail[0]} sent you an email from ${splitEmail[1]}`;
-  console.log(splitEmail, message);
+    return message;
+  }
 }
-const email = "en.nhshaongmail.com";
-sendNotification(email);
+const email = "sadia8icloud.com";
+console.log(sendNotification(email));
